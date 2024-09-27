a9db6cc58feb5a5535ff9d36e311059786132ab4
rickmff <rickmff@gmail.com>
2024-09-27 06:06:03 +0100

    keyburn 1.0

--- vite.config.ts
+++ vite.config.ts
@@ -19,23 +19,6 @@ export default defineConfig({
   build: {
     outDir: path.resolve(__dirname, "dist"),
     emptyOutDir: true,
-    assetsDir: "assets",
-    rollupOptions: {
-      output: {
-        chunkFileNames: "assets/js/[name]-[hash].js",
-        entryFileNames: "assets/js/[name]-[hash].js",
-        assetFileNames: ({ name }) => {
-          if (/\.(gif|jpe?g|png|svg)$/.test(name ?? "")) {
-            return "assets/images/[name]-[hash][extname]"
-          }
-          if (/\.css$/.test(name ?? "")) {
-            return "assets/css/[name]-[hash][extname]"
-          }
-          return "assets/[name]-[hash][extname]"
-        }
-      }
-    },
-    sourcemap: false,
-    minify: "esbuild"
+    assetsDir: "assets"
   }
 })
